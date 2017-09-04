7a1d8e095462464634add4a57ee4e49eff7af842
Maria Grigorieva <magsend@gmail.com>
2017-09-04 20:48:01 +0300

    First Lesson: - ignore node_modules/ - using CSV package - change colors of the CSV text in a cycle

---
+++ index.js
@@ -0,0 +1,28 @@
+const ansi = require('ansi');
+const cursor = ansi(process.stdout);
+const colors = require('colors');
+var csv = require('csv');
+
+cursor.beep();
+
+cols = [ ["\x1b[31m","\x1b[39m"], ["\x1b[32m","\x1b[39m"], ["\x1b[34m","\x1b[49m"] ];
+var index = 0; 
+for (index = 0; index < cols.length; index++ ) {
+	(function(i) {
+		csv.generate({seed: 1, columns: 2, length: 20}, function(err, data){
+		  csv.parse(data, function(err, data){
+		    csv.transform(data, function(data){
+		      return data.map(function(value){return value.toUpperCase()});
+		    }, function(err, data){
+			      	csv.stringify(data, function(err, data){
+				      	var lines = data.split('\n'); 
+				      	for (var j=0; j<lines.length; j++) {
+				        	console.log(cols[i][0]+lines[j]+cols[i][1]);
+				      	}
+			     	});
+		   		 });
+		 	});
+		 });
+	})(index)
+}
+
